5f1b7d4800e58139aa1d827e62d12c2078b4fe95
Andrew Chin <correctbutnotright@outlook.com>
2020-08-24 00:35:44 -0400

    urxvtc not recognizing ENV, using default location as temp fix

--- .config/slstatus/config.h
+++ .config/slstatus/config.h
@@ -69,6 +69,6 @@ static const struct arg args[] = {
 	{ battery_remaining, "[%s%] |", "BAT0" },
 	/* { disk_perc, " D %s%%", "/" }, */
 	{ run_command, " V %s |", "/home/andrew/bin/alsa/volume.sh" },
-	{ wifi_essid, " %.16s |", "wlp4s0" },
+	{ wifi_essid, " %.16s |", "wlp3s0" },
 	{ datetime, " %s", "%b %d %T %Z" },
 };
