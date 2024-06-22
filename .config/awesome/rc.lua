156868c0023be9cb4390d168d24293dff6e8b5ce
Andrew Chin <achin34@gatech.edu>
2024-06-22 13:39:26 -0400

    snapper systemd, wm tweaks

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -648,8 +648,8 @@ clientkeys = gears.table.join(
    -- awful.key({ modkey,           }, "o",      function (c) c:move_to_screen()               end,
    --    {description = "move to screen", group = "client"}),
    -- ANDREW what does 'end' do? Can bind this to another key later
-   -- awful.key({ modkey,           }, "t",      function (c) c.ontop = not c.ontop            end,
-   --    {description = "toggle keep on top", group = "client"}),
+   awful.key({ modkey,           }, "t",      function (c) c.ontop = not c.ontop            end,
+      {description = "toggle keep on top", group = "client"}),
    awful.key({ modkey,           }, "n",
       function (c)
          -- The client currently has the input focus, so it cannot be
