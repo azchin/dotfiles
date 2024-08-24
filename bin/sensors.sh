0b45e90029bee723445af3ad2f58e09cda1528d6
Andrew Chin <correctbutnotright@outlook.com>
2024-08-24 12:40:23 -0400

    misc, less options, print hex

--- bin/sensors.sh
+++ bin/sensors.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 sensors | sed -E '/it87/,+18d'
-cpupower frequency-info | sed "13s/  //;13q;d"
+cpupower frequency-info | sed "12s/  //;12q;d"
 # using this to omit useless sensors (may have messed up hardware)
