bf08ef7be3fca06382fb64fbd4ccd3da31f9d9ac
Andrew Chin <correctbutnotright@outlook.com>
2024-10-06 15:58:26 -0400

    upgrade sensors.sh

--- bin/sensors.sh
+++ bin/sensors.sh
@@ -1,4 +1,11 @@
-#!/bin/sh
-sensors | sed -E '/it87/,+18d'
-cpupower frequency-info | sed "12s/  //;12q;d"
-# using this to omit useless sensors (may have messed up hardware)
+#!/usr/bin/env bash
+get_info() {
+    sensors | sed -E '/it87/,+18d'
+    cpupower frequency-info | sed "12s/  //;12q;d"
+    # using this to omit useless sensors (may have messed up hardware)
+    gpu_dpm_sclk="/sys/class/drm/card1/device/pp_dpm_sclk"
+    echo -e "\nGPU clock"
+    cat $gpu_dpm_sclk
+}
+export -f get_info
+watch -n1 get_info
