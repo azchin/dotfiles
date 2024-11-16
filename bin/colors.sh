6ddb80c0758027def85c478a9ce5510e77ba9036
Andrew Chin <correctbutnotright@outlook.com>
2024-11-16 01:09:51 -0500

    colors

--- bin/colors.sh
+++ bin/colors.sh
@@ -1,2 +1,24 @@
-#!/bin/bash
-(x=`tput op` y=`printf %76s`;for i in {0..15};do o=00$i;echo -e ${o:${#o}-3:3} `tput setaf $i;tput setab $i`${y// /=}$x;done)
+#!/usr/bin/env bash
+set -eu
+foo() {
+    i=$1
+    colour=$2
+    o=00$i
+    width=14
+    prefix=""
+    if [ $i -ge 8 ]; then
+        prefix="Bright"
+        width=$((width - 6))
+    fi
+    printf "${o:${#o}-3:3} ${prefix}%${width}s " "$colour"
+    echo -e `tput setaf $i; tput setab $i`${y// /=}$x
+}
+
+x=`tput op` 
+y=`printf %76s`
+colors=("Black" "Red" "Green" "Yellow" "Blue" "Magenta" "Cyan" "White")
+for i in {0..15}; do 
+    j=$((i % 8))
+    colour=${colors[$j]}
+    foo $i "$colour"
+done
